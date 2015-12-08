25b69a3e524050c58670bc46c0ca762c6deec6f0
Tung HOANG <tung.hoang@gmail.com>
2015-12-08 11:30:31 +0700

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+if [ $# -lt 1 ]; then
+echo "
+Usage:
+    ./build.sh [0|1]
+        0: don't do cross compile for arm
+        1: do cross compile for arm
+"
+exit
+fi
+
 #define constant variable
 LINUX_ARMV7L=$PWD/../libs/linux-armv7l
 LINUX_X86_64=$PWD/../libs/linux-x86_64
@@ -6,17 +17,20 @@ LINUX_I686=$PWD/../libs/linux-i686
 MINGW=$PWD/../libs/mingw32-i586
 MACOS=$PWD/../libs/darwin-x86_64
 EXT=""
+ARM=$1
 #MACOS
 uname -a | grep "Darwin"
 if [ $? == 0 ]; then
 	INSTALL_DIR=$MACOS
 	EXT="x86_64-apple-darwin12.5.0"
+    echo "#include <pj/config_site_sample.h>" > pjlib/include/pj/config_site.h
 fi
 #MINGW
 uname -a | grep "MINGW32"
 if [ $? == 0 ]; then
 	INSTALL_DIR=$MINGW
 	EXT="i586-pc-mingw32"
+    echo "#include <pj/config_site_sample.h>" > pjlib/include/pj/config_site.h
 fi
 #Linux
 uname -a | grep "Linux"
@@ -29,21 +43,29 @@ if [ $? == 0 ]; then
 		INSTALL_DIR=$LINUX_X86_64
 		EXT="x86_64-unknown-linux-gnu"
 	fi
+    echo "
+#include <pj/config_site_sample.h>
+#define PJMEDIA_AUDIO_DEV_HAS_PORTAUDIO 0
+#define PJMEDIA_AUDIO_DEV_HAS_ALSA 1
+" > pjlib/include/pj/config_site.h
 fi
 
-#build for arm first
-rm -rf $LINUX_ARMV7L/include/pj*
-rm -rf $LINUX_ARMV7L/lib/libpj*
-rm -rf $LINUX_ARMV7L/lib/libg7221codec-arm-none-linux-gnueabi.a
-rm -rf $LINUX_ARMV7L/lib/libgsmcodec-arm-none-linux-gnueabi.a
-rm -rf $LINUX_ARMV7L/lib/libilbccodec-arm-none-linux-gnueabi.a
-rm -rf $LINUX_ARMV7L/lib/libportaudio-arm-none-linux-gnueabi.a
-rm -rf $LINUX_ARMV7L/lib/libresample-arm-none-linux-gnueabi.a
-rm -rf $LINUX_ARMV7L/lib/libspeex-arm-none-linux-gnueabi.a
-make distclean
-./configure --host=arm-none-linux-gnueabi --target=arm-none-linux-gnueabi --prefix=$LINUX_ARMV7L CFLAGS=-I$LINUX_ARMV7L/include LDFLAGS=-L$LINUX_ARMV7L/lib
-make -j4
-make install
+if [ $ARM == 1 ]; then
+        #build for arm first
+        rm -rf $LINUX_ARMV7L/include/pj*
+        rm -rf $LINUX_ARMV7L/lib/libpj*
+        rm -rf $LINUX_ARMV7L/lib/libg7221codec-arm-none-linux-gnueabi.a
+        rm -rf $LINUX_ARMV7L/lib/libgsmcodec-arm-none-linux-gnueabi.a
+        rm -rf $LINUX_ARMV7L/lib/libilbccodec-arm-none-linux-gnueabi.a
+        rm -rf $LINUX_ARMV7L/lib/libportaudio-arm-none-linux-gnueabi.a
+        rm -rf $LINUX_ARMV7L/lib/libresample-arm-none-linux-gnueabi.a
+        rm -rf $LINUX_ARMV7L/lib/libspeex-arm-none-linux-gnueabi.a
+        make distclean
+        ./configure --host=arm-none-linux-gnueabi --target=arm-none-linux-gnueabi --prefix=$LINUX_ARMV7L CFLAGS=-I$LINUX_ARMV7L/include LDFLAGS=-L$LINUX_ARMV7L/lib
+        make dep
+        make
+        make install
+fi
 
 #build for intel
 rm -rf $INSTALL_DIR/include/pj*
